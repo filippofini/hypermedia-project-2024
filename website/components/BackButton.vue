99888bc72fdfec7b79a7c1e10d657a203604d551
TheKingOjama <126713432+TheKingOjama@users.noreply.github.com>
2024-05-21 20:06:37 +0200

    added basic back button

---
+++ website/components/BackButton.vue
@@ -0,0 +1,44 @@
+<template>  
+    <button class="button-container">
+        <a href="" class="back-button" @click.prevent="$router.back()" role="button" aria-label="Back button">&#10554;</a>  
+    </button>
+</template>
+
+<style scoped>
+    button {
+        border: none;        
+        cursor: pointer;
+    }
+
+    .button-container {        
+        width: 7vh;
+        height: 7vh;
+        margin: 0.5%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        background-color:  #B96090;
+        transition: all 0.2s ease-out;
+    }
+
+    .back-button {
+        color: white;
+        font-weight: bolder;
+        font-size: xx-large;
+        border-radius: 50%;
+        text-decoration: none;
+        
+              
+        
+    }
+
+    .button-container:hover {
+        background-color: #6B5AA1;
+    }
+
+    .button-container:active {
+        background-color: #52417D;
+    }
+</style>
+
