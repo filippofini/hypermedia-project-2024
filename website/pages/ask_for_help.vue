0b6630ca0dc141e94f5755008784579aea3ca2de
LucaLonginotti <luca.longinotti@mail.polimi.it>
2024-05-29 17:26:52 +0200

    Added breadcrumps

--- website/pages/ask_for_help.vue
+++ website/pages/ask_for_help.vue
@@ -1,3 +1,20 @@
 <template>
-    
+    <BreadCrumbs :crumbs="BCrumbs"/>
 </template>
+
+<script>
+  export default {
+      computed: {
+          BCrumbs() {                
+              return [
+
+                  {
+                      label: 'home',
+                      url: '/'
+                  }
+                  
+              ];
+          }
+      }
+  }
+</script>
--- website/pages/ask_for_help_for_another.vue
+++ website/pages/ask_for_help_for_another.vue
@@ -1,3 +1,26 @@
 <template>
-    
+      <BreadCrumbs :crumbs="BCrumbs"/>
 </template>
+
+<script>
+    export default {
+        computed: {
+            BCrumbs() {                
+                return [
+
+                    {
+                        label: 'home',
+                        url: '/',
+                    },
+
+                    { 
+                        label: 'ask for help',
+                        url: '/ask_for_help'
+
+                    }
+                    
+                ];
+            }
+        }
+    }
+</script>
