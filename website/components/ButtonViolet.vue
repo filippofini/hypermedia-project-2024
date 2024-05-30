49d288a27aef177b434559151b633d37594cce87
LucaLonginotti <luca.longinotti@mail.polimi.it>
2024-05-30 13:55:59 +0200

    Update ButtonViolet.vue

--- website/components/ButtonViolet.vue
+++ website/components/ButtonViolet.vue
@@ -14,19 +14,21 @@ export default {
 <style scoped>
 .buttonViolet{
     background-color: #52417D;
-    padding: 1vw;
+    padding: 0.7vw 2vw;
     font-size: 1.2vw; 
     font-family: 'Jost'; 
     display: flex;
     justify-content: flex-end;
-    border: 0;
     color: white;
     margin: 0;
     transition: background-color 0.3s ease;
+    border-radius: 1vw;
+    text-decoration: none;
+    
 }
 
 .buttonViolet:hover {
-    background-color: #6B5AA1; 
-    cursor: pointer;
+  background-color: #7D5BA6;
+  cursor: pointer;
 }
 </style>
