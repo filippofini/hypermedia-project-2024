b171e7304973fe8bce51d00261d808c219ac7414
TheKingOjama <126713432+TheKingOjama@users.noreply.github.com>
2024-06-28 14:49:53 +0200

    404 page

---
+++ website/error.vue
@@ -0,0 +1,61 @@
+<template>    
+    <NuxtLayout>
+        <div class="error-area">
+            <div class="error-header">                   
+                <div class="error-404">404</div>  
+            </div>
+            <div class="title-page-1-5">Sorry, we couldn't find the page you where looking for...</div><br>
+            
+                
+                <div class="dx">                    
+                    <div class="error-text">You can visit our website at:</div>
+                    <ButtonViolet name_button = "Homepage" link="/"></ButtonViolet>
+                </div>
+            
+
+        </div> 
+    </NuxtLayout>            
+
+</template>
+
+
+
+<style>
+
+    .error-header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        overflow: hidden;
+    }
+
+    
+    .error-area {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 78.5vh;
+        width: 100%;
+    }
+    .dx {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    @media screen and (max-width: 800px) {
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            margin-top: 10vh;
+        }
+    }
+
+</style>
